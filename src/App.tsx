6e5ca6a6f56c9a6d2cf144701cd977167e4d13db
Justin Beaudry <beaudry.justin@gmail.com>
2025-02-28 23:10:01 -0500

    use more diverse stock photo

--- src/App.tsx
+++ src/App.tsx
@@ -169,7 +169,7 @@ function App() {
         <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
           <img
             className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-            src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
+            src="stock_photo.png"
             alt="Team collaborating on grant proposal"
           />
         </div>
